2013b08f1fb967be607a69d399775e0017b20f9f
Just-A-Programer <laimisjure@gmail.com>
2025-11-17 22:05:48 +0200

    HideUI

--- Assets/CODE/UI/hideUI.cs
+++ Assets/CODE/UI/hideUI.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Net.NetworkInformation;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -9,19 +7,16 @@ public class hideUI : MonoBehaviour
 {
     public bool HideUI = true;
     public GameObject canvas;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(Input.KeyCode.h))
+        if (Input.GetKeyDown(KeyCode.H))
         {
             HideUI = !HideUI;
             canvas.SetActive(HideUI);
+            Cursor.visible = HideUI;
         }
     }
 }
